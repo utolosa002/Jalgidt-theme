de1d0b0f7b576e26e120d621a3ec26b4127c61e6
Tolosa <tolosa@talaios.net>
2020-04-01 16:36:49 +0200

    [update] remove front-page widgets

--- front-page.php
+++ front-page.php
@@ -10,14 +10,31 @@ $home_sections = blossom_travel_get_home_sections();
 if ( 'posts' == get_option( 'show_on_front' ) ) { //Show Static Blog Page
     include( get_home_template() );
 }elseif( $home_sections ){ 
-    get_header();
-    //If any one section are enabled then show custom home page.
-    echo '<div id="content" class="site-content">';
-    foreach( $home_sections as $section ){
-        get_template_part( 'sections/' . esc_attr( $section ) );  
-    }
-    echo '</div>';
-    get_footer();
+    get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+			<?php
+			while ( have_posts() ) : the_post();
+
+				get_template_part( 'template-parts/content', 'page' );
+
+				/**
+                 * Comment Template
+                 * 
+                 * @hooked blossom_travel_comment
+                */
+                do_action( 'blossom_travel_after_page_content' );
+
+			endwhile; // End of the loop.
+			?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();
 }else {
     //If all section are disabled then show respective page template. 
     include( get_page_template() );
